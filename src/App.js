592e454778a909588fa393bee4e2d3ec0d718f3d
Katarina Ninkovic <katarinaninkovic00@gmail.com>
2023-01-22 15:40:14 +0100

    Kreiran Footer,Pocetna i dodate rute i css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Pocetna from './components/Pocetna';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
     <BrowserRouter>
         <NavBar></NavBar>
+        <Routes>
+            <Route path="/" element={<Pocetna></Pocetna>}></Route>
+            /* ovde se posle dodaju rute za Restorane i omiljene*/
+          </Routes>
      </BrowserRouter>
+     <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Pocetna.jsx
@@ -0,0 +1,34 @@
+function Pocetna() {
+    return (
+      <div  className='pocetna'>
+
+      <div className="pocetna-text">
+          <h1  > Pronadjite najbolje restorane u Beogradu</h1>
+      </div>
+
+      <br /><br /><br /><br /><br />
+      <div className="flex-container">
+          <div>
+              <h2>Zadatak:</h2>
+              <p> Ukoliko tražite idealan restoran za poslovni ručak, proslavu Vama bitnih događaja ili romantičnu večeru
+                 sa dragom osobom, ovde ćete pronaći najbolji. U Beogradu postoji veliki broj svih vrsta restorana. 
+                
+                 </p>
+          </div>
+          <div> 
+          <h2>Informacije: </h2>
+              <p> 
+                 Bavimo se promocijom restorana od 2019. godine. Preporučićemo samo one restorane gde je najbolja i najraznovrsnija kuhinja. 
+                 Uvek ćete biti informisani koji restorani imaju promotivne cene hrane i pića. 
+                 U bilo kom trenutku možete pogledati da li Vam odgovaraju cene koje se kreću u tim restoranima, kao i njihova lokacija. 
+                 Lako ćete dobiti sve potrebne informacije ako budete pratili naše smernice. </p>
+          </div>
+
+        </div>  
+
+    </div>
+
+    );
+  }
+
+  export default Pocetna;
